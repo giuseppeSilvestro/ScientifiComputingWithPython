5abec986743682e43507a94f522cef248f0d4622
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2021-05-02 10:48:20 +0100

    Fix issues

--- WebScrapingWithBeautifulSoup.py
+++ WebScrapingWithBeautifulSoup.py
@@ -1,8 +1,11 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
+# Create a variable with the url of the website you want to scrap
 url = 'http://www.dr-chuck.com/page1.htm'
+# create a connection with the website using urllib package
 html = urllib.request.urlopen(url).read()
+# create an object using BeautifulSoup where to store the html in that page
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags in the html page
