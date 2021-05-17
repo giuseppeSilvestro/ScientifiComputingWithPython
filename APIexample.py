da07ebcafb4622e9094ba9f38dbbbabe73e58ffd
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2021-05-17 17:28:14 +0100

    Class and Object

--- APIexample.py
+++ APIexample.py
@@ -10,7 +10,8 @@ while True:
     address = input("Enter location: ")
     if len(address) < 1: break
 
-    url = serviceurl + urllib.parse.urlencode({'address': address})
+    url = serviceurl + urllib.parse.urlencode(
+        {'address': address})
 
     print("Retrieving", url)
     uh = urllib.request.urlopen(url)
---
+++ SimpleClass.py
@@ -0,0 +1,14 @@
+# Here is a simple example of Class and Object
+
+class PartyAnimal:
+    x = 0
+
+    def party(self):
+        self.x = self.x + 1
+        print("So far", self.x)
+
+an = PartyAnimal()
+
+an.party()
+an.party()
+an.party()
