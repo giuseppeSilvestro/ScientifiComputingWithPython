d0c167157b4f11eac05c2d3c82f7d723c3a7bee9
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2021-05-17 17:31:08 +0100

    Class and Object

--- SimpleClass.py
+++ SimpleClass.py
@@ -12,3 +12,9 @@ an = PartyAnimal()
 an.party()
 an.party()
 an.party()
+
+# check what type our variable is
+print("Type", type(an))
+# check what methods and attribute our variable class has
+# the results with underscore are used by python and you can ignore them
+print("Dir", dir(an))
